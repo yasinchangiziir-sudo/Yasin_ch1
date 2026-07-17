78baaa43a930be34a8d0d8e48bb5087047eeb9c7
yasinchangiziir-sudo <yasinchangizi.ir@gmail.com>
2026-07-17 22:49:40 +0330

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ import edge_tts
 # -------------------- Configuration --------------------
 TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "8910769488:AAG7effUIZqoK0vVLJ_zRAVJ7K4ifgMX4AY")
 ADMIN_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "@Oython")
-ADMIN_USER_ID = int(os.environ.get("ADMIN_USER_ID", "8910769488"))
+ADMIN_USER_ID = int(os.environ.get("ADMIN_USER_ID", "8391932958"))
 ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "admin123")
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY", "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")
 DATABASE = "victims.db"
