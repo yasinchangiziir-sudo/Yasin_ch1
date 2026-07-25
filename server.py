9f656a222d3502f8b34ed387121c8e33ea59f7bb
yasinchangiziir-sudo <yasinchangizi.ir@gmail.com>
2026-07-25 22:03:43 +0330

    Update server.py

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "8910769488:AAG7effUIZqoK0vVLJ_zRAV
 ADMIN_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "@Oython")
 ADMIN_USER_ID = int(os.environ.get("ADMIN_USER_ID", "8391932958"))
 ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "admin123")
-GROQ_API_KEY = os.environ.get("GROQ_API_KEY", "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")
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY", "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")
 DATABASE = "victims.db"
 PUBLIC_URL = os.environ.get("RENDER_EXTERNAL_URL", "https://your-app.onrender.com")
 
